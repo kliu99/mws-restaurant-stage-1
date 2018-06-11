06ac467a6d71670b65e7cfcc24dc73326edf85bc
Kai Liu <kliu99@gatech.edu>
2018-06-11 16:02:19 -0400

    update sw

--- sw.js
+++ sw.js
@@ -36,14 +36,7 @@ self.addEventListener('activate', (event) => {
 // support offline
 self.addEventListener('fetch', function(event) {
     var requestUrl = new URL(event.request.url);
-  
-    // if (requestUrl.origin === location.origin) {
-    //   if (requestUrl.pathname === '/') {
-    //     event.respondWith(caches.match('/skeleton'));
-    //     return;
-    //   }
-    // }
-  
+
     event.respondWith(
       caches.match(event.request).then(function(response) {
         return response || fetch(event.request);
