9ee94cf3a6984f96e603c10e4fac6df2e98bed4e
Kai Liu <kliu99@gatech.edu>
2018-06-12 09:06:19 -0400

    breadcrumb

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -174,7 +174,13 @@ createReviewHTML = (review) => {
 fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
-  li.innerHTML = restaurant.name;
+
+  const a = document.createElement('a');
+  a.innerHTML = restaurant.name;
+  a.href="#";
+  a.setAttribute("aria-current", "page");
+  li.appendChild(a);
+
   breadcrumb.appendChild(li);
 }
 
