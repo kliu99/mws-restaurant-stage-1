c080b46babf987f0d717fc8184be12644c9dbf89
Kai Liu <kliu99@gatech.edu>
2018-06-13 08:05:32 -0400

    make changes to address reviewer's comments

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -196,9 +196,23 @@ class DBHelper {
 
 }
 
-
 /* IndexedDb */
 const db = new Dexie('RestaurantReviews');
 db.version(1).stores({
   restaurants: 'id'
 })
+
+/**
+ * Register Server worker
+ */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+      navigator.serviceWorker.register('/sw.js').then(function (registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function (err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+} 
