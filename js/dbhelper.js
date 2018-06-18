7c7dc804eb9835ebbdf44c6b6086a2df8d65e7ba
Kai Liu <kliu99@gatech.edu>
2018-06-17 21:41:03 -0700

    bug fixed

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -202,3 +202,19 @@ const db = new Dexie('RestaurantReviews');
 db.version(1).stores({
   restaurants: 'id'
 })
+
+
+/**
+ * Register Server worker
+ */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+      navigator.serviceWorker.register('/sw.js').then(function (registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function (err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+} 
